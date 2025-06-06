27320f2c987daddc4f032d21ecce222199b18778
chiparon <823002174@qq.com>
2025-06-07 02:03:32 +0800

    feat: 添加记录时默认使用当前日期和时间

--- main.cpp
+++ main.cpp
@@ -1,37 +1,13 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
-#include "financemanager.h"
-#include "remindermanager.h"
-#include "passwordmanager.h"
+#include <QApplication>
+#include "mainwindow.h"
 
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
-
-    // 注册自定义类型
-    qmlRegisterType<FinanceManager>("App", 1, 0, "FinanceManager");
-    qmlRegisterType<ReminderManager>("App", 1, 0, "ReminderManager");
-    qmlRegisterType<PasswordManager>("App", 1, 0, "PasswordManager");
-
-    QQmlApplicationEngine engine;
-
-    // 创建管理器实例
-    FinanceManager financeManager;
-    ReminderManager reminderManager;
-    PasswordManager passwordManager;
-
-    // 设置上下文属性
-    engine.rootContext()->setContextProperty("financeManager", &financeManager);
-    engine.rootContext()->setContextProperty("reminderManager", &reminderManager);
-    engine.rootContext()->setContextProperty("passwordManager", &passwordManager);
-
-    // 加载主QML文件
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
-    if (engine.rootObjects().isEmpty()) {
-        return -1;
-    }
-
-    return app.exec();
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    
+    // 创建并显示主窗口
+    MainWindow mainWindow;
+    mainWindow.show();
+    
+    // 运行应用程序事件循环
+    return QApplication::exec();
 }
